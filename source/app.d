f4bb465f4d723c45617fdf1928ff6279bb42f878
me@igiag.net <me@igiag.net>
2024-03-18 16:53:13 +0100

    Removed unnececary imports

--- source/app.d
+++ source/app.d
@@ -4,10 +4,6 @@ import std.json;
 
 import raven.document_store;
 
-import jsonizer.tojson;
-
-import person;
-
 void main()
 {
 	DocumentStore store = new DocumentStore("http://10.0.0.14:8080","Zsyp");
